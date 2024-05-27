ab3b947e7db5b680944ba0d15149c064dbf90ab1
山本　太陽 <yamamototaiyou.811@gmail.com>
2024-05-27 23:52:53 +0900

    編集ページ

---
+++ 会員登録システム/resources/views/edit/edit.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container">
+        <h1>会員編集  会員ID:{{$member->id}}</h1>
+        
+        <form action="{{ url('')}}" method="POST">
+            @csrf
+            <input type="text" name="name" id="user-name" value="{{ $member->name }}"><br>
+            <input type="tel" name="tel" id="user-tel" value="{{ $member->tel }}"><br>
+            <input type="text" name="email" id="user-email" value="{{ $member->email }}"><br>
+            <button type=submit>登録</button>
+        </form>
+
+        <form action="{{ url('')}}" method="POST">
+            @csrf
+            @method('DELETE');
+            <button type=submit>登録</button>
+        </form>
+
+    </div>
+
+</body>
+</html>
