3c8c944ca84a4deef49f6e9168a989dcb8bbc831
山本　太陽 <yamamototaiyou.811@gmail.com>
2024-05-28 15:47:03 +0900

    編集画面

--- 会員登録システム/resources/views/edit/edit.blade.php
+++ 会員登録システム/resources/views/edit/edit.blade.php
@@ -10,18 +10,19 @@
     <div class="container">
         <h1>会員編集  会員ID:{{$member->id}}</h1>
         
-        <form action="{{ url('')}}" method="POST">
+        <form action="{{ url('member/'.$member->id)}}" method="POST">
             @csrf
+            @method('PUT')
             <input type="text" name="name" id="user-name" value="{{ $member->name }}"><br>
             <input type="tel" name="tel" id="user-tel" value="{{ $member->tel }}"><br>
             <input type="text" name="email" id="user-email" value="{{ $member->email }}"><br>
-            <button type=submit>登録</button>
+            <button type=submit>編集</button>
         </form>
 
-        <form action="{{ url('')}}" method="POST">
+        <form action="{{ url('member/'.$member->id)}}" method="POST">
             @csrf
-            @method('DELETE');
-            <button type=submit>登録</button>
+            @method('DELETE')
+            <button type=submit>削除</button>
         </form>
 
     </div>
